24da50cf12e9011cd5cb936dd5557323d6f286fa
luke-h1 <luke.howsam@yahoo.com>
2022-07-05 12:34:30 +0100

    repositories

---
+++ src/server/src/main/java/com/mikro/blog/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.mikro.blog.repository;
+
+import com.mikro.blog.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}
