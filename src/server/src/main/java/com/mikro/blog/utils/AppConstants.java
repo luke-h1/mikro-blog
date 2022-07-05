465a3f47b4998d84baf897535a2577fa9406d01f
luke-h1 <luke.howsam@yahoo.com>
2022-07-05 13:09:43 +0100

    utils & security config start

---
+++ src/server/src/main/java/com/mikro/blog/utils/AppConstants.java
@@ -0,0 +1,8 @@
+package com.mikro.blog.utils;
+
+public class AppConstants {
+  public static final String DEFAULT_PAGE_NUMBER = "0";
+  public static final String DEFAULT_PAGE_SIZE = "10";
+  public static final String DEFAULT_SORT_BY = "createdAt";
+  public static final String DEFAULT_SORT_DIRECTION = "asc";
+}
